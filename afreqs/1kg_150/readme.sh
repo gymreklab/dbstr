13c5853f85ff56e24fb3a90dbfd2d2fa378fd03c
gymreklab <melissa.gymrek@gmail.com>
2019-05-22 11:31:12 -0700

    adding allele freq scripts

---
+++ afreqs/1kg_150/readme.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for pop in eur afr eas
+do
+    echo ${pop}
+    for chrom in $(seq 1 22)
+    do
+	echo ${pop} ${chrom}
+	VCFFILE=/storage/s1saini/manuscript_strsnp/fig3/hipstr.1kg/${pop}/chr${chrom}/hipstr.chr${chrom}.${pop}.vcf.gz
+	./parse_1kg_afreqs.py ${VCFFILE}
+    done >  /storage/mgymrek/webstr_files/afreqs/1kg_${pop}_afreqs.csv
+done
